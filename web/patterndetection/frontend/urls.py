0296e30a37c6ac8c3d1d75c5d0116c0dcb9943a1
smith-tg <smithtg@email.arizona.edu>
2020-04-17 09:27:17 -0500

    routing updates

--- web/patterndetection/frontend/urls.py
+++ web/patterndetection/frontend/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
 	path('patterndetection/', views.index),
 	path('patterndetection/textfile/', views.prealigned_text_file),
 	path('patterndetection/peptidelist/', views.text_file_peptide_list),
+	path('', RedirectView.as_view('https://langelab.med.ubc.ca/'))
 ]
