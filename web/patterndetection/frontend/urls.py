1cb894e8ea10f929284e250a89233c8f452af5ab
smith-tg <smithtg@email.arizona.edu>
2020-04-17 09:32:04 -0500

    routing updates

--- web/patterndetection/frontend/urls.py
+++ web/patterndetection/frontend/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 	path('patterndetection/', views.index),
 	path('patterndetection/textfile/', views.prealigned_text_file),
 	path('patterndetection/peptidelist/', views.text_file_peptide_list),
-	path('', RedirectView.as_view('https://langelab.med.ubc.ca/'))
+	path('', RedirectView.as_view(url='https://langelab.med.ubc.ca/')),
 ]
