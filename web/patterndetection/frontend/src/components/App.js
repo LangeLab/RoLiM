4c9f929374a05c39ed3e2b98eaf2f8c03356ab41
smith-tg <smithtg@email.arizona.edu>
2020-04-29 10:12:02 -0500

    styling updates

--- web/patterndetection/frontend/src/components/App.js
+++ web/patterndetection/frontend/src/components/App.js
@@ -8,7 +8,7 @@ const App = () => (
   <div className="container">
   	<div>
   		<h1 class="title">RoLiM</h1>
-  		<h2 class="subtitle">Robust detection of linear motifs in sequence data.</h2>
+  		<h2 class="st">Robust detection of linear motifs in sequence data.</h2>
   	</div>
   	<br />
   	<JobForm endpoint="/api/job/" />
