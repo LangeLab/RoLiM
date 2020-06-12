5cc84041e522054dab7d4589600b89d3cb5697c1
smith-tg <smithtg@email.arizona.edu>
2020-06-12 17:07:00 -0500

    bug fixes

--- web/patterndetection/frontend/src/components/App.js
+++ web/patterndetection/frontend/src/components/App.js
@@ -7,7 +7,7 @@ import JobForm from "./JobForm";
 const App = () => (
   <div className="container">
   	<header>
-  		<h1 class="lablink"><a href="https://langelab.med.ubc.ca/">Lange Lab</a></h1>
+  		<h1 class="title"><a class="lablink" href="https://langelab.med.ubc.ca/">Lange Lab</a></h1>
   		<h2 class="title">RoLiM</h2>
   		<h3 class="subtitle">Robust detection of linear motifs in sequence data.</h3>
   	</header>
