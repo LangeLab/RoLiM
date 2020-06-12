dc130b4af738e438b1c2f6deaf13995ab1c4bada
smith-tg <smithtg@email.arizona.edu>
2020-06-12 13:36:21 -0500

    bug fixes

--- web/patterndetection/tool/models.py
+++ web/patterndetection/tool/models.py
@@ -71,13 +71,13 @@ class Job(models.Model):
 	compound_residue_decomposition = models.BooleanField(blank=True, default=True)
 	position_specific = models.BooleanField(blank=True, default=True)
 	require_context_id = models.BooleanField(blank=True, default=True)
-	#redundancylevel = models.ForeignKey('RedundancyLevel',
-										#on_delete=models.CASCADE,
-										#default=DEFAULT_REDUNDANCYLEVEL)
+	redundancylevel = models.ForeignKey('RedundancyLevel',
+										on_delete=models.CASCADE,
+										default=DEFAULT_REDUNDANCYLEVEL)
 	first_protein_only = models.BooleanField(blank=True, default=True)
-	#originalrowmerge = models.ForeignKey('OriginalRowMerge',
-											#on_delete=models.CASCADE,
-											#default=DEFAULT_ORIGINALROWMERGE)
+	originalrowmerge = models.ForeignKey('OriginalRowMerge',
+											on_delete=models.CASCADE,
+											default=DEFAULT_ORIGINALROWMERGE)
 
 class ForegroundFormat(models.Model):
 	"""Supported foreground data set format options."""
