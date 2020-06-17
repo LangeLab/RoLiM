6fa357f487843abb43b74d6a9acb6aefd111bdb6
smith-tg <smithtg@email.arizona.edu>
2020-06-17 18:46:32 -0500

    compatibility notice

--- web/patterndetection/frontend/src/components/App.js
+++ web/patterndetection/frontend/src/components/App.js
@@ -22,7 +22,6 @@ const App = () => (
   	<br />
   	<main>
   		<span className="compatibility">*Browser compatibility notice: RoLiM works best when used with Firefox, Chrome, or Internet Explorer.</span>
-  		<br />
   		<section>
   			<h4 className="title is-4 description-header">Description</h4>
   			<p className="description">RoLiM iteratively detects over-represented linear motifs in sequence data sets.</p>
