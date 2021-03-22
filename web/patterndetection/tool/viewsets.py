1e319bac63f8f03f04c93ddc407a6edee3cc5c4d
smith-tg <smithtg@email.arizona.edu>
2021-03-22 16:44:37 -0500

    increase job timeout

--- web/patterndetection/tool/viewsets.py
+++ web/patterndetection/tool/viewsets.py
@@ -51,7 +51,7 @@ class JobViewSet(viewsets.ModelViewSet):
 
 		# Add job to task queue.
 		queue = django_rq.get_queue('default')
-		queue.enqueue(new_job, args=(self.jobcode,), job_timeout='24h')
+		queue.enqueue(new_job, args=(self.jobcode,), job_timeout='48h')
 
 
 class ForegroundFormatViewSet(viewsets.ModelViewSet):
